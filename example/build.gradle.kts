d16781f1abd0254708b30261f2cc2e5f13a71056
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-02-02 13:38:50 +0100

    Add example project

---
+++ example/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.github.g3force.oc")
+}
+
+oc {
+    // Set the path to the oc binary manually or let it be detected from your path
+//    ocBinary = File("${System.getenv("user.home")}/.local/bin/oc")
+    clusterUrl = "https://your.cnap.cluster:443"
+    projectName = "your-project"
+    // Set either a token file or the token itself. The tokenFile takes precedence, if it exists
+    tokenFile = File("service-account.token")
+//    token = "secret-token"
+}
+
+// Apply all .yaml files in the templates directory
+tasks.register<com.github.g3force.oc.OcApplyTask>("ocApply") {
+    source = fileTree("${projectDir}/templates") {
+        include("*.yaml")
+    }
+}
