7950d59e53ca9f7fb5809dd4702f12a953fce036
Kindrat <legioner.alexei@gmail.com>
2020-02-24 17:07:53 +0200

    Added login via plain text credentials

--- src/main/kotlin/com/github/g3force/oc/OcLoginTask.kt
+++ src/main/kotlin/com/github/g3force/oc/OcLoginTask.kt
@@ -8,11 +8,24 @@ abstract class OcLoginTask : OcExecTask() {
     @TaskAction
     override fun doAction() {
         val config = project.extensions.getByType(OcPluginExtension::class.java)
-        val tokenFile = config.tokenFile
-        if (tokenFile != null && tokenFile.exists()) {
-            config.token = tokenFile.readText()
-        }
+        val loginArgs = mutableListOf("login", config.clusterUrl)
+        loginArgs.addAll(getLoginArgs(config))
+        execute(loginArgs)
+    }
 
-        execute(listOf("login", config.clusterUrl, "--token", config.token))
+    private fun getLoginArgs(config: OcPluginExtension): List<String> {
+        val tokenFile = config.tokenFile
+        config.token?.let {
+            return listOf("--token", it)
+        }
+        if (tokenFile != null && tokenFile.exists()) {
+            return listOf("--token", tokenFile.readText())
+        }
+        val credentials = config.credentials
+        if (!credentials.isEmpty()) {
+            return listOf("-u", credentials.username, "-p", credentials.password)
+        }
+        logger.info("No authorisation data provided")
+        return emptyList()
     }
 }
